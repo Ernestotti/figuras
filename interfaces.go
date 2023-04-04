5a03e888ab814612ab847c6e74ec5d3e8a13db42
Ernestotti <ernesto_13186@hotmail.com>
2023-04-04 16:16:18 +0200

    Correción metodo area

--- interfaces.go
+++ interfaces.go
@@ -2,12 +2,12 @@ package figuras
 
 import "fmt"
 
-type Geometria interface {
+type geometria interface {
 	area() float64
 	perimetro() float64
 }
 
-func Medidas(g Geometria) {
+func Medidas(g geometria) {
 	fmt.Println("Medidas:", g)
 	fmt.Println("Area:", g.area())
 	fmt.Println("Perimetro:", g.perimetro())
